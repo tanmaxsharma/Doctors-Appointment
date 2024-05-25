d1ad325be25bec7ea4445e4145832cc5b1d11efe
tanmaxsharma <tanmaysharma.eng05@gmail.com>
2024-05-25 22:44:39 +0530

    initial commit

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,58 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useContext, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard.jsx";
+import Login from "./components/Login.jsx";
+import AddNewDoctor from "./components/AddNewDoctor.jsx";
+import Messages from "./components/Messages.jsx";
+import Doctors from "./components/Doctors.jsx";
+import { Context } from "./main";
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Sidebar from "./components/Sidebar.jsx";
+import AddNewAdmin from "./components/AddNewAdmin.jsx";
+import "./App.css";
+
+const App = () => {
+  // eslint-disable-next-line no-unused-vars
+  const { isAuthenticated, setIsAuthenticated, setAdmin } =
+    useContext(Context);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/admin/me",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true);
+        setAdmin(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setAdmin({});
+      }
+    };
+    fetchUser();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated]);
+
+  return (
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/doctor/addnew" element={<AddNewDoctor />} />
+        <Route path="/admin/addnew" element={<AddNewAdmin />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/doctors" element={<Doctors />} />
+      </Routes>
+      <ToastContainer position="top-center" />
+    </Router>
+  );
+};
+
+export default App;
